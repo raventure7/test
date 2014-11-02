be9414363880195f8764cee824df0a2cd75a87bd
raventure7 <raventure7@gmail.com>
2014-11-02 15:14:14 +0900

    update#1

---
+++ web.js
@@ -0,0 +1,23 @@
+﻿/*
+var http = require('http');
+var socketio = require("socket.io");
+var server = http.createServer(function (request, response){
+	response.end('helllow NodeJS');
+}).listen(8001, function(){
+	console.log('test server');
+});
+*/
+// 모듈 추출
+
+var fs = require('fs');
+var ejs = require('ejs');
+var mysql = require('mysql');
+var http = require('http');
+var server = require('http').createServer(function (request, response){
+	response.end('helllow NodeJS');
+});
+var io = require('socket.io').listen(server);
+io.set('log level', 1);
+server.listen(8001, function(){
+	console.log('Server running');
+});
