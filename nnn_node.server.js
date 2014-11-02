a9a31a9980ecd171417f96fcf17ded7247a4266f
raventure7 <raventure7@gmail.com>
2014-11-02 14:14:38 +0900

    server
    server

--- nnn_node.server.js
+++ nnn_node.server.js
@@ -1,24 +1,7 @@
-﻿// 모듈 추출
-var fs = require('fs');
-var ejs = require('ejs');
-var mysql = require('mysql');
-
-//서버생성
-var server = require('http').createServer();
-var io = require('socket.io').listen(server);
-
-//DB 연결
-/*
-var client = mysql.createConnection({
-	host: 'http://creatorz15.cafe24.com',
-	user: 'creatorz15',
-	password : 'ckdwhwk7564' ,
-	database : 'creatorz15'
+﻿var http = require('http');
+var socketio = require("socket.io");
+var server = http.createServer(function (request, response){
+	response.end('helllow NodeJS');
+}).listen(8001, function(){
+	console.log('test_server');
 });
-*/
-
-io.set('log level', 1);
-server.listen(52273, function(){
-	console.log('Server running at http://127.0.0.1:52273');
-});
-
