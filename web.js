3144976a0f6e678c8b3dd9f1ad55355881156fe4
raventure7 <raventure7@gmail.com>
2014-11-02 14:20:06 +0900

    module update
    test

--- web.js
+++ web.js
@@ -3,5 +3,5 @@ var socketio = require("socket.io");
 var server = http.createServer(function (request, response){
 	response.end('helllow NodeJS');
 }).listen(8001, function(){
-	console.log('sesrver');
+	console.log('test esrver');
 });
