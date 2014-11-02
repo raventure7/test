ed0a0bdd3c015c15cae920bec591f67d85d45927
raventure7 <raventure7@gmail.com>
2014-11-02 14:49:06 +0900

    new commit
    server update

--- nnn_node.server.js
+++
@@ -1,7 +0,0 @@
-﻿var http = require('http');
-var socketio = require("socket.io");
-var server = http.createServer(function (request, response){
-	response.end('helllow NodeJS');
-}).listen(8001, function(){
-	console.log('test_server');
-});
